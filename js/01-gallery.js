858ded33ae9d3e1828c5d41dabc5603553099e7c
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-03 13:32:53 +0100

    gallerry 01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
+const list = document.querySelector('.gallery');
+
+function renderList(images) {
+    const markup = images.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}">
+        </a>
+        </li>`
+    }).join('');
+
+    list.innerHTML = markup;
+};
+
+renderList(galleryItems);
+
+list.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+      const source = event.target.parentElement.getAttribute('href');
+  
+      const instance = basicLightbox.create(`<img src="${source}" alt="Image">`, {
+        onShow: (instance) => {
+          instance.element().querySelector('img').setAttribute('src', source);
+        }
+      });
+  
+      instance.show();
+    }
+  });
+  
 console.log(galleryItems);
