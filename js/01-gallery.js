2d54293ded24916ff163f4743155d41078779134
il0naa <136170246+il0naa@users.noreply.github.com>
2023-11-03 14:04:37 +0100

    lightbox 2-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,7 @@ function renderList(images) {
     const markup = images.map(({preview, original, description}) => {
         return `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
-            <img class="gallery__image" src="${preview}" alt="${description}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
         </a>
         </li>`
     }).join('');
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const list = document.querySelector('.gallery');
+
+function renderList(images) {
+    const markup = images.map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+              <img class="gallery__image" src="${preview}" alt="${description}">
+          </a>
+      </li>`;
+    }).join('');
+  
+    list.innerHTML = markup;
+  };
+
+  renderList(galleryItems);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionDelay: 250,
+  });
 
 console.log(galleryItems);
